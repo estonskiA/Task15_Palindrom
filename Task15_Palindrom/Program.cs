de1939f2413ee8f8f58d2bc1f90b907d37a2da83
Анна Карасик <1032202194@pfur.ru>
2020-10-27 08:35:00 +0300

    Добавьте файлы проекта.

---
+++ Task15_Palindrom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task15_Palindrom
+{
+    class Palindrom 
+    { 
+       public void palindrom_check()
+       {
+            Console.WriteLine("Введите предложение без знаков препинания, сэр: ");
+            string st = Console.ReadLine();
+            st = Regex.Replace(st, @"\s+", "").ToLower();
+            int st_length = st.Length;
+            int flag = 1;
+            for (int i = 0; i < st_length/2 + 1; i = i + 1)
+            {
+                if (st[i] != st[st_length - i - 1])
+                {
+                    Console.WriteLine("Не в этот раз, но... не теряйте надежду!");
+                    flag = 0;
+                    break;
+                }
+            }
+
+            if (flag == 1)
+            {
+                Console.WriteLine("Это он! Ура!");
+            }
+        }  
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Palindrom();
+            p.palindrom_check();
+
+        }
+    }
+}
+
+
